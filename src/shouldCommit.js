5e915ffe929f68c11ea31555319593165072da7d
Max Brodheim <max.brodheim@gmail.com>
2017-08-05 16:10:02 -0400

    adds training file

--- src/shouldCommit.js
+++ src/shouldCommit.js
@@ -18,23 +18,47 @@ function makeTest(num) {
   let result = [];
   for (let i = num; i > 0; i--) {
     result.push({
-      insertions: Math.random(),
-      deletions: Math.random(),
-      filesChanged: Math.random(),
-      time: Math.random()
+      input: {
+        insertions: Math.random(),
+        deletions: Math.random(),
+        filesChanged: Math.random(),
+        time: Math.random()
+      },
+      output: {
+        yes: 1,
+        no: 1
+      }
     });
   }
   return result;
 }
 
+console.log(makeTest(40));
+
 function hashTime(timeInMinutes) {
   if (timeInMinutes > 100) timeInMinutes = 100;
   return Math.log10(timeInMinutes / 10);
 }
+
+function reverseHashTime(num) {
+  return Math.floor(Math.pow(10, num) * 10);
+}
+
 function hashFilesChanged(num) {
   if (num > 10) return 1;
-  return num / 10;
+  return Math.log10(num);
 }
-function hashInsertions(num) {
 
+function reverseHashFilesChanged(num) {
+  return Math.pow(10, num);
+}
+
+function hashInsertionsDeletions(num) {
+  if (num > 100) return 1;
+  return Math.log10(num / 10);
+}
+
+function reverseInsDel(num) {
+  if (num > 100) num = 100;
+  return Math.log10(num / 10);
 }
---
+++ src/training.js
@@ -0,0 +1,519 @@
+let tests = [{
+    input: {
+      insertions: 0.14821708693817737,
+      deletions: 0.7628270601794502,
+      filesChanged: 0.9697356981687155,
+      time: 0.9612226768572689
+    },
+    output: {
+      yes: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.9764238590050776,
+      deletions: 0.48937756341007854,
+      filesChanged: 0.1716322126013896,
+      time: 0.7275140335916057
+    },
+    output: {
+      yes: 1,
+    }
+  },
+  {
+    input: {
+      insertions: 0.262692296364031,
+      deletions: 0.19550951078684808,
+      filesChanged: 0.8943005778505673,
+      time: 0.2437656626685374
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.5511534553413975,
+      deletions: 0.9958387835987395,
+      filesChanged: 0.9889514984960501,
+      time: 0.7587030223610356
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.9289973898766934,
+      deletions: 0.3023066802794754,
+      filesChanged: 0.4301108191465588,
+      time: 0.18633533119798673
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.8773199446478857,
+      deletions: 0.8606426226313157,
+      filesChanged: 0.08339126036319588,
+      time: 0.973814455199145
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.7365024346256588,
+      deletions: 0.751473787898808,
+      filesChanged: 0.10230134702418225,
+      time: 0.5686282935447866
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.06764538991453084,
+      deletions: 0.1961750002422089,
+      filesChanged: 0.5348118636837218,
+      time: 0.1768387659384285
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.6305034053283334,
+      deletions: 0.5981854325459752,
+      filesChanged: 0.21918425003109476,
+      time: 0.8784240791652669
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.82041336434245,
+      deletions: 0.7492734276845798,
+      filesChanged: 0.6179356358767698,
+      time: 0.8992629036516178
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.3807254557761759,
+      deletions: 0.16643727417980947,
+      filesChanged: 0.4178670242711273,
+      time: 0.5142337978189171
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.22877620118298658,
+      deletions: 0.501497129223613,
+      filesChanged: 0.568386160208509,
+      time: 0.26719579898094903
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.4398026175013594,
+      deletions: 0.7388723367738534,
+      filesChanged: 0.0492263707836369,
+      time: 0.3233107312917274
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.6970846860870428,
+      deletions: 0.6535021910960215,
+      filesChanged: 0.924439547220048,
+      time: 0.5847772162027116
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.6457540987616346,
+      deletions: 0.4599025729758419,
+      filesChanged: 0.536624458539902,
+      time: 0.08478784814686025
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.8455184592234783,
+      deletions: 0.7598178388073975,
+      filesChanged: 0.36348781982797607,
+      time: 0.6028752768009791
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.7652003212595233,
+      deletions: 0.8361074672772917,
+      filesChanged: 0.5921958066851916,
+      time: 0.26597041002458544
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.030898585732074046,
+      deletions: 0.041700215599812296,
+      filesChanged: 0.026342986645316424,
+      time: 0.5524321118582574
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.10756545976223286,
+      deletions: 0.2694276845648267,
+      filesChanged: 0.0752597054995705,
+      time: 0.9181513925030094
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.9209283312711445,
+      deletions: 0.7662902139290528,
+      filesChanged: 0.9226331542715127,
+      time: 0.5476233197066087
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.9278269799603829,
+      deletions: 0.1221885994580767,
+      filesChanged: 0.6269888076566126,
+      time: 0.4619931045425385
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.45449499765783763,
+      deletions: 0.02716599728287794,
+      filesChanged: 0.5181333987001047,
+      time: 0.5625402996773254
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.5420401675572872,
+      deletions: 0.09710012344544361,
+      filesChanged: 0.31247743961661967,
+      time: 0.29822701453732825
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.20057936293447276,
+      deletions: 0.41398125864445445,
+      filesChanged: 0.8094528450002985,
+      time: 0.48162016330333235
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.920811385065913,
+      deletions: 0.6935145680520094,
+      filesChanged: 0.2006041470576223,
+      time: 0.8046570620598987
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.5060210764454771,
+      deletions: 0.049969274026441246,
+      filesChanged: 0.924712137878366,
+      time: 0.6155028627303747
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.9266253988497886,
+      deletions: 0.4169326853328259,
+      filesChanged: 0.5376479394861589,
+      time: 0.6282709249372311
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.6027598951904825,
+      deletions: 0.7789131469623753,
+      filesChanged: 0.7732499218323223,
+      time: 0.5769569203809473
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.014356332705653951,
+      deletions: 0.11171606971679715,
+      filesChanged: 0.6677887959847557,
+      time: 0.3688497896781793
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.3187067053231374,
+      deletions: 0.9979895434814832,
+      filesChanged: 0.9526329898873902,
+      time: 0.6501278061788445
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.09741278496730543,
+      deletions: 0.5665552754468026,
+      filesChanged: 0.4015276351128414,
+      time: 0.4404775630450686
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.10568129008047733,
+      deletions: 0.2921483726744325,
+      filesChanged: 0.17796456752922207,
+      time: 0.4133487298131957
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.9946666649208877,
+      deletions: 0.7295604770648012,
+      filesChanged: 0.9259935444886473,
+      time: 0.007831910867860836
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.23661050661172456,
+      deletions: 0.6087423659301543,
+      filesChanged: 0.7613016763911153,
+      time: 0.6913308377570229
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.29929117659184756,
+      deletions: 0.8441386493822176,
+      filesChanged: 0.3315225429302233,
+      time: 0.7850091653962303
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.3381758464088962,
+      deletions: 0.5137265960232449,
+      filesChanged: 0.788201739383902,
+      time: 0.49197568868237584
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.796204908219976,
+      deletions: 0.46186747226745783,
+      filesChanged: 0.8270480366799036,
+      time: 0.5643031646019474
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.5496420908771438,
+      deletions: 0.5952538694445313,
+      filesChanged: 0.2818402913319933,
+      time: 0.18120211176033307
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.10952093124907059,
+      deletions: 0.7475218431531672,
+      filesChanged: 0.618850709447136,
+      time: 0.4768439719738171
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  },
+  {
+    input: {
+      insertions: 0.45601765488425827,
+      deletions: 0.5462975880642655,
+      filesChanged: 0.9900750963310001,
+      time: 0.7908254382472817
+    },
+    output: {
+      yes: 1,
+      no: 1
+    }
+  }
+];
+function hashTime(timeInMinutes) {
+  if (timeInMinutes > 100) timeInMinutes = 100;
+  return Math.log10(timeInMinutes / 10);
+}
+
+function reverseHashTime(num) {
+  return Math.floor(Math.pow(10, num) * 10);
+}
+
+function hashFilesChanged(num) {
+  if (num > 10) return 1;
+  return Math.log10(num);
+}
+
+function reverseHashFilesChanged(num) {
+  return Math.pow(10, num);
+}
+
+function hashInsDel(num) {
+  if (num > 100) num = 100;
+  return Math.log10(num / 10);
+}
+
+function reversehashInsDel(num) {
+  return Math.floor(Math.pow(10, num) * 10);
+}
+
+function convertTests(tests) {
+  for (let i = 0; i < tests.length; i++) {
+    console.log(`${i}
+
+
+
+                 Time: ${reverseHashTime(tests[i].input.time)}
+                 Deletions: ${reverseInsDel(tests[i].input.deletions)}
+                 Insetions: ${reverseInsDel(tests[i].input.insertions)}
+                 Files Changed: ${reverseHashFilesChanged(tests[i].input.filesChanged)}\n`);
+  }
+}
+convertTests(tests);
